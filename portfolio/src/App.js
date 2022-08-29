b7160030b793648bf6642df17e49f6291b8d072e
idtaleb <reda.idtaleb.etu@univ-lille.Fr>
2022-08-29 20:29:21 +0200

    project structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import Profile from "./portfolioContainer/Home/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/portfolioContainer/Home/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href="https://www.linkedin.com/in/r%C3%A9daid-taleb/" rel="noopener noreferrer" target="_blank">
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+
+                        <a href="https://github.com/reda-idtaleb" rel="noopener noreferrer" target="_blank">
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
